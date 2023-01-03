43de0df479dc0f37182f2b459de8884ced1f3353
Emmy-Akintz <emzyakints2005@gmail.com>
2023-01-03 09:50:41 +0100

    Update

--- src/components/New-Release-Carousel.jsx
+++ src/components/New-Release-Carousel.jsx
@@ -6,7 +6,7 @@ function NewReleaseCarousel() {
     return (
         <>
             <h1 className="font-bold text-xl text-left text-white">New releases.</h1>
-            <div className="block md:flex lg:flex justify-between mt-4 text-white text-center md:text-left lg:text-left hover:animate-scale">
+            <div className="block md:flex lg:flex justify-between mt-4 text-white text-center md:text-left lg:text-left">
                 {library.map((e) => (
                     <div className="block m-2">
                         <img src={e.image} alt="" className="m-auto" />
